e37ddaf0627a33454921dc46486f744fd286281e
patri <patri@172.16.0.44>
2019-07-08 17:21:18 +0200

    First commit

---
+++ miam-miam-project/src/main/java/io/ldnr/miamProject/miammiamproject/MiamMiamProjectApplication.java
@@ -0,0 +1,13 @@
+package io.ldnr.miamProject.miammiamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiamMiamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiamMiamProjectApplication.class, args);
+	}
+
+}
